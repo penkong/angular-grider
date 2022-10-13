7553930d31d55ce31d6ce9022e0a9d713754d1da
Mazdak <nazemi.works@gmail.com>
2022-10-13 12:55:32 +0200

    more on

--- card-comp/src/app/app.component.ts
+++ card-comp/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'card-comp';
+  posts = [
+    {
+      title: 'neat tree',
+      username: '@nature',
+      imgUrl: 'assets/tree.jpeg',
+      content: 'i saw it today',
+    },
+    {
+      title: 'moutain',
+      username: '@mountain',
+      imgUrl: 'assets/mountain.jpeg',
+      content: 'i asw it yesterday',
+    },
+    {
+      title: 'biking',
+      username: '@biking',
+      imgUrl: 'assets/biking.jpeg',
+      content: 'i will it tomorrow',
+    },
+  ];
 }
