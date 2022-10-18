9e207c2551b5425d545a72e6611991386aa8f58c
Mazdak Nazemi <nazemi.works@gmail.com>
2022-10-18 06:26:22 -0700

    more on

---
+++ modularproj/src/app/collections/collection-home/collection-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-home',
+  templateUrl: './collection-home.component.html',
+  styleUrls: ['./collection-home.component.scss']
+})
+export class CollectionHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- modularproj/src/app/elements/elements.module.ts
+++ modularproj/src/app/elements/elements.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ElementsHomeComponent
+  ],
   imports: [
     CommonModule,
     ElementsRoutingModule
+  ],
+  exports: [
   ]
 })
 export class ElementsModule { }
