bf2af64fdb6186ea077cdec31356da9f3414030b
Mazdak Nazemi <nazemi.works@gmail.com>
2022-10-18 23:26:04 -0700

    more on

---
+++ modularproj/src/app/views/statistics/statistics.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.scss']
+})
+export class StatisticsComponent implements OnInit {
+
+  @Input() data: Array<any>= []
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- modularproj/src/app/views/views-home/views-home.component.ts
+++ modularproj/src/app/views/views-home/views-home.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./views-home.component.scss']
 })
 export class ViewsHomeComponent implements OnInit {
-
+  stats = [
+    { value: 34 , label: '# of Users'},
+    {value: 43 , label: 'Revenue'},
+    { value: 3453, label: 'Reviews'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
