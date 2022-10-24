ef0c71d91becd8462ca2eeb950f0921fe2da63c0
Mazdak Nazemi <nazemi.works@gmail.com>
2022-10-24 15:53:57 +0200

    more on

---
+++ authemailapp/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomInputComponent } from './custom-input/custom-input.component';
+
+
+
+@NgModule({
+  declarations: [
+    CustomInputComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
